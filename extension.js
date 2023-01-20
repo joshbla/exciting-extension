06b8180530f7123f5e7355caea4d8f4d348645c7
Joshua Blake Smith <jblakezsmith@gmail.com>
2023-01-19 21:19:57 -0700

    Initial commit

---
+++ extension.js
@@ -0,0 +1,20 @@
+// Replace all varitation of boring with exciting
+document.body.innerHTML = document.body.innerHTML.replace(/bor·ing/g, 'ex·cit·ing');
+
+document.body.innerHTML = document.body.innerHTML.replace(/bore·dom/g, 'ex·cite·ment');
+
+document.body.innerHTML = document.body.innerHTML.replace(/boring/g, 'exciting');
+
+document.body.innerHTML = document.body.innerHTML.replace(/Boring/g, 'Exciting');
+
+document.body.innerHTML = document.body.innerHTML.replace(/boredom/g, 'excitement');
+
+document.body.innerHTML = document.body.innerHTML.replace(/Boredom/g, 'Excitement');
+
+document.body.innerHTML = document.body.innerHTML.replace(/bored/g, 'excited');
+
+document.body.innerHTML = document.body.innerHTML.replace(/Bored/g, 'Excited');
+
+document.body.innerHTML = document.body.innerHTML.replace(/bore/g, 'excite');
+
+document.body.innerHTML = document.body.innerHTML.replace(/Bore/g, 'Excite');
